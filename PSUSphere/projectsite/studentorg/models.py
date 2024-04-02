ed646c933bc834d2e16d5716de13f3e9f102195d
Nick Francis Bundal <nnikeetmg@gmail.com>
2024-04-02 08:55:15 +0800

    studentorg in settings.py

--- PSUSphere/projectsite/studentorg/models.py
+++ PSUSphere/projectsite/studentorg/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Basemodel (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated+at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    class College(BaseModel):
+        college_name = models.CharField(max_length=150)
+
+        def__str__(self):
+            return self.college_name
+    
+    class Program(BaseModel):
+        prog_name = models.CharField
